a39a30aa94f0a01a1ac1ccf590a98d8cfc204dc0
luciism <lucism.dev@gmail.com>
2025-01-23 10:27:32 +1300

    feat: add smooth scrolling

--- lua/plugins/cursor.lua
+++ lua/plugins/cursor.lua
@@ -1,24 +1,82 @@
 return {
-{
-  "brenton-leighton/multiple-cursors.nvim",
-  version = "*",  -- Use the latest tagged version
-  opts = {},  -- This causes the plugin setup function to be called
-  keys = {
-    {"<C-j>", "<Cmd>MultipleCursorsAddDown<CR>", mode = {"n", "x"}, desc = "Add cursor and move down"},
-    {"<C-k>", "<Cmd>MultipleCursorsAddUp<CR>", mode = {"n", "x"}, desc = "Add cursor and move up"},
+    {
+        "brenton-leighton/multiple-cursors.nvim",
+        version = "*", -- Use the latest tagged version
+        opts = {}, -- This causes the plugin setup function to be called
+        keys = {
+            {
+                "<C-j>",
+                "<Cmd>MultipleCursorsAddDown<CR>",
+                mode = { "n", "x" },
+                desc = "Add cursor and move down",
+            },
+            { "<C-k>",  "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "x" },      desc = "Add cursor and move up" },
 
-    {"<C-Up>", "<Cmd>MultipleCursorsAddUp<CR>", mode = {"n", "i", "x"}, desc = "Add cursor and move up"},
-    {"<C-Down>", "<Cmd>MultipleCursorsAddDown<CR>", mode = {"n", "i", "x"}, desc = "Add cursor and move down"},
+            { "<C-Up>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "i", "x" }, desc = "Add cursor and move up" },
+            {
+                "<C-Down>",
+                "<Cmd>MultipleCursorsAddDown<CR>",
+                mode = { "n", "i", "x" },
+                desc = "Add cursor and move down",
+            },
 
-    {"<C-LeftMouse>", "<Cmd>MultipleCursorsMouseAddDelete<CR>", mode = {"n", "i"}, desc = "Add or remove cursor"},
+            {
+                "<C-LeftMouse>",
+                "<Cmd>MultipleCursorsMouseAddDelete<CR>",
+                mode = { "n", "i" },
+                desc = "Add or remove cursor",
+            },
 
-    {"<Leader>a", "<Cmd>MultipleCursorsAddMatches<CR>", mode = {"n", "x"}, desc = "Add cursors to cword"},
-    {"<Leader>A", "<Cmd>MultipleCursorsAddMatchesV<CR>", mode = {"n", "x"}, desc = "Add cursors to cword in previous area"},
+            {
+                "<Leader>a",
+                "<Cmd>MultipleCursorsAddMatches<CR>",
+                mode = { "n", "x" },
+                desc = "Add cursors to cword",
+            },
+            {
+                "<Leader>A",
+                "<Cmd>MultipleCursorsAddMatchesV<CR>",
+                mode = { "n", "x" },
+                desc = "Add cursors to cword in previous area",
+            },
 
-    {"<Leader>d", "<Cmd>MultipleCursorsAddJumpNextMatch<CR>", mode = {"n", "x"}, desc = "Add cursor and jump to next cword"},
-    {"<Leader>D", "<Cmd>MultipleCursorsJumpNextMatch<CR>", mode = {"n", "x"}, desc = "Jump to next cword"},
+            {
+                "<Leader>d",
+                "<Cmd>MultipleCursorsAddJumpNextMatch<CR>",
+                mode = { "n", "x" },
+                desc = "Add cursor and jump to next cword",
+            },
+            { "<Leader>D", "<Cmd>MultipleCursorsJumpNextMatch<CR>", mode = { "n", "x" }, desc = "Jump to next cword" },
 
-    {"<Leader>l", "<Cmd>MultipleCursorsLock<CR>", mode = {"n", "x"}, desc = "Lock virtual cursors"},
-  },
-},
+            {
+                "<Leader>l",
+                "<Cmd>MultipleCursorsLock<CR>",
+                mode = { "n", "x" },
+                desc = "Lock virtual cursors",
+            },
+        },
+    },
+    {
+        "karb94/neoscroll.nvim",
+        opts = {},
+        config = function()
+            require("neoscroll").setup({
+                mappings = { -- Keys to be mapped to their corresponding default scrolling animation
+                    "<C-u>",
+                    "<C-d>",
+                    "<C-b>",
+                    "<C-f>",
+                    "<C-y>",
+                    "<C-e>",
+                    "zt",
+                    "zz",
+                    "zb",
+                    "gg",
+                    "G"
+                },
+                duration_multiplier = 0.3,
+                easing = "quadratic",
+            })
+        end,
+    },
 }
