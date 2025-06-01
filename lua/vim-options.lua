50e75c40e1f6d4e48e7cee250b82e142e2e4e4c8
Luciism <lucism.dev@gmail.com>
2025-06-02 11:34:49 +1200

    add pretty diagnostics

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,12 +1,13 @@
 vim.cmd("set number")
 vim.cmd("set relativenumber")
 
-
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set number relativenumber")
+
+vim.diagnostic.config({ virtual_text = false })
 
 -- vim.cmd("Neotree toggle left")
 vim.g.mapleader = " "
