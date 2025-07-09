b3d661ac21e1d34d1bf15cca746311918a5a3c1e
Luciism <lucism.dev@gmail.com>
2025-07-09 15:45:19 +1200

    add whichkey

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -16,5 +16,8 @@ return {
             { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
             { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
         },
+    },
+    {
+        "tpope/vim-obsession"
     }
 }
